3903c895eff86385b8604e4bdfef9402c9ca8861
Parshva Runwal <runwalparshva3@gmail.com>
2024-04-13 23:33:12 +0530

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import catalogueRouter from "./routes/catalogue.router";
 import kycRouter from "./routes/kyc.router";
 import productRouter from "./routes/product.router";
 import { storeRouter } from "./routes/store.router";
-import productRouter from "./routes/product.router";
+import cors from "cors";
 
 dotenv.config();
 
@@ -15,19 +15,25 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 declare global {
-    namespace Express {
-        interface Request {
-            clerkUserId?: string;
-        }
+  namespace Express {
+    interface Request {
+      clerkUserId?: string;
     }
+  }
 }
 
 loadConstants();
 
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: "*",
+  })
+);
 app.use(express.json());
 
 app.get("/ping", (_req: Request, res: Response) => {
-    return res.send({ status: "success", message: "pong!" });
+  return res.send({ status: "success", message: "pong!" });
 });
 
 app.use("/api/v1/kyc", verifyJWT, kycRouter);
@@ -38,5 +44,5 @@ app.use("/api/v1/catalogue", verifyJWT, catalogueRouter);
 app.use("/api/v1/product", verifyJWT, productRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
