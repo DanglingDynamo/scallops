28104f6cd4695820b89e45af555a868a2231f431
DanglingDynamo <vedant.matanhelia@gmail.com>
2024-04-13 22:26:47 +0530

    feat: bigint can't serialize

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { accountRouter } from "./routes/account.router";
 import catalogueRouter from "./routes/catalogue.router";
 import kycRouter from "./routes/kyc.router";
 import { storeRouter } from "./routes/store.router";
+import productRouter from "./routes/product.router";
 
 dotenv.config();
 
@@ -13,11 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 declare global {
-  namespace Express {
-    interface Request {
-      clerkUserId?: string;
+    namespace Express {
+        interface Request {
+            clerkUserId?: string;
+        }
     }
-  }
 }
 
 loadConstants();
@@ -25,7 +26,7 @@ loadConstants();
 app.use(express.json());
 
 app.get("/ping", (_req: Request, res: Response) => {
-  return res.send({ status: "success", message: "pong!" });
+    return res.send({ status: "success", message: "pong!" });
 });
 
 app.use("/api/v1/kyc", verifyJWT, kycRouter);
@@ -33,7 +34,8 @@ app.use("/api/v1/kyc", verifyJWT, kycRouter);
 app.use("/api/v1/", storeRouter);
 app.use("/api/v1/", accountRouter);
 app.use("/api/v1/catalogue", verifyJWT, catalogueRouter);
+app.use("/api/v1/product", verifyJWT, productRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { addProduct, listProducts } from "../controllers/product.controller";
 import { deleteProduct } from "../services/product.service";
+import multer from "multer";
 
 const productRouter = Router();
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 // Get all products for a store
 productRouter.get("/", listProducts);
 
 // Add a product to a store
-productRouter.post("/", addProduct);
+productRouter.post("/", upload.single("file"), addProduct);
 
 // Delete a product from a store
 productRouter.delete("/:productId", deleteProduct);
