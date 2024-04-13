8c6f381b8ad9d53f5f03bd83bbcd62283ba42397
DanglingDynamo <vedant.matanhelia@gmail.com>
2024-04-13 22:42:36 +0530

    feat: prisma just SUCKS

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -4,37 +4,42 @@ import { v4 as uuidv4 } from "uuid";
 import { findStore } from "../services/store.service";
 
 export default class AccountController {
-  async addAccount(req: Request, res: Response) {
-    try {
-      const store = await findStore(req.clerkUserId);
+    async addAccount(req: Request, res: Response) {
+        try {
+            const store = await findStore(req.clerkUserId);
 
-      if (!store) {
-        return res.status(404).json({
-          status: "fail",
-          data: { store: "user does not have a store " },
-        });
-      }
+            if (!store) {
+                return res.status(404).json({
+                    status: "fail",
+                    data: { store: "user does not have a store " },
+                });
+            }
 
-      const storeID = store.id;
+            const storeID = store.id;
 
-      const exists = (await findAccount(String(storeID))) ? true : false;
-      if (exists) {
-        return res.status(409).json({
-          status: "fail",
-          data: { account: "account already exists " },
-        });
-      }
+            const exists = (await findAccount(String(storeID))) ? true : false;
+            if (exists) {
+                return res.status(409).json({
+                    status: "fail",
+                    data: { account: "account already exists " },
+                });
+            }
 
-      const currBalance = parseInt(req.body.curr_balance);
-      if (isNaN(currBalance)) {
-        res.status(422).json({ status: "fail", data: { curr_balance: "not a number" } });
-      }
+            const currBalance = parseInt(req.body.curr_balance);
+            if (isNaN(currBalance)) {
+                res
+                    .status(422)
+                    .json({ status: "fail", data: { curr_balance: "not a number" } });
+            }
 
-      const accountNumber = parseInt(req.body.account_number);
-      if (isNaN(accountNumber)) {
-        res.status(422).json({ status: "fail", data: { curr_balance: "not a number" } });
-      }
+            const accountNumber = parseInt(req.body.account_number);
+            if (isNaN(accountNumber)) {
+                res
+                    .status(422)
+                    .json({ status: "fail", data: { curr_balance: "not a number" } });
+            }
 
+<<<<<<< Updated upstream
       const account = await addAccount({
         balance: currBalance,
         beneficiary: String(req.body.beneficiary),
@@ -47,15 +52,31 @@ export default class AccountController {
         createdAt: new Date(),
         updatedAt: new Date(),
       });
+=======
+            const account = await addAccount({
+                balance: currBalance,
+                beneficiary: String(req.body.beneficiary),
+                ifsc_code: String(req.body.ifsc_code),
+                account_number: BigInt(accountNumber),
+                id: uuidv4(),
+                storeID: String(storeID),
+                branch: String(req.body.branch),
+                bank_name: String(req.body.bank_name),
+                createdAt: new Date(),
+                updatedAt: null,
+            });
+>>>>>>> Stashed changes
 
-      return res.status(500).json({
-        status: "success",
-        message: "account created successfully",
-        data: { account: account },
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ status: "fail", message: "internal server error" });
+            return res.status(500).json({
+                status: "success",
+                message: "account created successfully",
+                data: { account: account },
+            });
+        } catch (error) {
+            console.log(error);
+            return res
+                .status(500)
+                .json({ status: "fail", message: "internal server error" });
+        }
     }
-  }
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,21 +1,44 @@
 import prisma from "../../prisma";
 
 export async function getProducts(storeId: string, page: number) {
-  return await prisma.product.findMany({
-    where: { storeID: storeId },
-    take: 10,
-    skip: (page - 1) * 10,
-  });
+    return await prisma.product.findMany({
+        where: { storeID: storeId },
+        take: 10,
+        skip: (page - 1) * 10,
+    });
 }
 
+<<<<<<< Updated upstream
 export async function createProduct(storeId: string, name: string, description: string, price: number, quantity: number, image: string, barcode: bigint) {
   return await prisma.product.create({
     data: { storeID: storeId, name, description, price, quantity, image, barcode: BigInt(barcode) },
   });
+=======
+export async function createProduct(
+    storeId: string,
+    name: string,
+    description: string,
+    price: number,
+    quantity: number,
+    image: string,
+    barcode: number,
+) {
+    return await prisma.product.create({
+        data: {
+            storeID: storeId,
+            name,
+            description,
+            price,
+            quantity,
+            image,
+            barcode,
+        },
+    });
+>>>>>>> Stashed changes
 }
 
 export async function deleteProduct(productId: string) {
-  return await prisma.product.delete({
-    where: { id: productId },
-  });
+    return await prisma.product.delete({
+        where: { id: productId },
+    });
 }
